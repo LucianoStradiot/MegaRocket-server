d44ca8ad7b9923358d6290fb046d68646907a58d
unknown <lstradiot15@gmail.com>
2023-07-25 13:25:16 -0300

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import app from './app';
+import express from 'express';
+import cors from 'cors';
+import router from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
 
 dotenv.config();
 
