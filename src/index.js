21c1bfe6af7c3300bc74985b2b001164393cfac9
unknown <lstradiot15@gmail.com>
2023-07-25 17:49:07 -0300

    Fixed the deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
-import router from './routes';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/api', router);
+import app from './app';
 
 dotenv.config();
 
@@ -34,5 +26,3 @@ mongoose.connect(process.env.DB_URL, { maxPoolSize: process.env.MONGO_POOLSIZE |
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-export default app;
